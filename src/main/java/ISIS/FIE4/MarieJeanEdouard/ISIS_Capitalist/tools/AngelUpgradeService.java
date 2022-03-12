eacc0904b9ec0b23bc857dbb5919d45b3ff9917c
jemon <jean-edouard.monnerville@etud.univ-jfc.fr>
2022-03-12 20:38:01 +0100

    Angel upgrade available.

--- src/main/java/ISIS/FIE4/MarieJeanEdouard/ISIS_Capitalist/tools/AngelUpgradeService.java
+++ src/main/java/ISIS/FIE4/MarieJeanEdouard/ISIS_Capitalist/tools/AngelUpgradeService.java
@@ -1,20 +1,74 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ISIS.FIE4.MarieJeanEdouard.ISIS_Capitalist.tools;
 
 import generated.PallierType;
+import generated.ProductType;
+import generated.World;
+import java.io.FileNotFoundException;
+import javax.xml.bind.JAXBException;
 
-/**
- *
- * @author jemon
- */
 public class AngelUpgradeService {
 
-    public void updateAngelUpgrade(PallierType newAngelUpgrade) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private WorldServices worldServices;
+
+    public AngelUpgradeService() {
+        worldServices = new WorldServices();
     }
-    
+
+    public boolean updateAngelUpgrade(PallierType newAngelUpgrade, String username) throws JAXBException, FileNotFoundException {
+        World world = worldServices.getWorld(username);
+        PallierType angelUpgrade = findAngelByName(username, world);
+        if (angelUpgrade == null) {
+            return false;
+        }
+
+        if (world.getActiveangels() > angelUpgrade.getSeuil()) {
+            //On soustrait le coût de l'ange
+            world.setTotalangels(world.getActiveangels() - angelUpgrade.getSeuil());
+            //On indique qu'il est déblocké
+            angelUpgrade.setUnlocked(true);
+            
+            //On applique son bonus
+            switch(angelUpgrade.getTyperatio()){
+                case GAIN:
+                    applyAngelUpgradeForAllGain(angelUpgrade, world);
+                    break;
+                case VITESSE:
+                    applyAngelUpgradeForAllVitesse(angelUpgrade, world);
+                    break;
+                case ANGE:
+                    applyAngeUpgrade(newAngelUpgrade, world);
+                    break;
+            }
+        }
+
+        worldServices.saveWorldToXml(world, username);
+        return true;
+    }
+
+    private PallierType findAngelByName(String name, World world) {
+        PallierType result = null;
+        for (PallierType pt : world.getAngelupgrades().getPallier()) {
+            if (pt.getName().equals(name)) {
+                result = pt;
+            }
+        }
+        return result;
+    }
+
+    public void applyAngelUpgradeForAllGain(PallierType ange, World world) {
+        for (ProductType product : world.getProducts().getProduct()) {
+            product.setRevenu(product.getRevenu() * ange.getRatio());
+        }
+    }
+
+    public void applyAngelUpgradeForAllVitesse(PallierType ange, World world) {
+        for (ProductType product : world.getProducts().getProduct()) {
+            product.setRevenu(product.getTimeleft() * ange.getRatio());
+        }
+    }
+
+    private void applyAngeUpgrade(PallierType ange, World world) {
+        world.setAngelbonus((int) (world.getAngelbonus() + ange.getRatio()));
+    }
+
 }
