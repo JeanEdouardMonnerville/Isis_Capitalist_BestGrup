159271914d0797899b130f873a9f53c95f70b775
jemon <jean-edouard.monnerville@etud.univ-jfc.fr>
2022-03-12 13:14:06 +0100

    Fonctionnalité getWorld patch

--- src/main/java/ISIS/FIE4/MarieJeanEdouard/ISIS_Capitalist/WebService/WebServiceWorld.java
+++ src/main/java/ISIS/FIE4/MarieJeanEdouard/ISIS_Capitalist/WebService/WebServiceWorld.java
@@ -3,6 +3,8 @@ package ISIS.FIE4.MarieJeanEdouard.ISIS_Capitalist.WebService;
 
 import ISIS.FIE4.MarieJeanEdouard.ISIS_Capitalist.tools.WorldServices;
 import generated.World;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -10,7 +12,9 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.xml.bind.JAXBException;
 
+@Path("generic")
 public class WebServiceWorld {
 
     WorldServices services;
@@ -22,11 +26,9 @@ public class WebServiceWorld {
     @GET
     @Path("world")
     @Produces({"application/xml", "application/json"})
-    public World getWorld(@Context HttpServletRequest request) {
-        //String username = request.getHeader("X-user");
-        //return services.getWorld(username);
-        return services.getWorld();
-
+    public World getWorld(@Context HttpServletRequest request) throws JAXBException, FileNotFoundException{
+        String username = request.getHeader("X-user");
+        return services.getWorld(username);
     }
 
     @DELETE
