1861841e8d2a6fea3e952c50acd8cfc720acd8d8
jemon <jean-edouard.monnerville@etud.univ-jfc.fr>
2022-03-12 20:39:47 +0100

    Angel upgrade available.

--- src/main/java/ISIS/FIE4/MarieJeanEdouard/ISIS_Capitalist/WebService/WebServiceAngelUpgrade.java
+++ src/main/java/ISIS/FIE4/MarieJeanEdouard/ISIS_Capitalist/WebService/WebServiceAngelUpgrade.java
@@ -2,11 +2,13 @@ package ISIS.FIE4.MarieJeanEdouard.ISIS_Capitalist.WebService;
 
 import ISIS.FIE4.MarieJeanEdouard.ISIS_Capitalist.tools.AngelUpgradeService;
 import generated.PallierType;
+import java.io.FileNotFoundException;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
+import javax.xml.bind.JAXBException;
 
 @Path("generic")
 public class WebServiceAngelUpgrade {
@@ -19,8 +21,9 @@ public class WebServiceAngelUpgrade {
     @PUT
     @Path("upgrade")
     @Consumes({"application/xml","application/json"})
-    public void updateAngelUpgrade(@Context HttpServletRequest request,PallierType newAngelUpgrade){
-        services.updateAngelUpgrade(newAngelUpgrade);
+    public void updateAngelUpgrade(@Context HttpServletRequest request,PallierType newAngelUpgrade) throws JAXBException, FileNotFoundException{
+        String username = request.getHeader("X-user");
+        services.updateAngelUpgrade(newAngelUpgrade,username);
     }
 }
  
