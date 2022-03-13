7c279c1a9e8525906e68693c40950aa2e5fc5c5d
jemon <jean-edouard.monnerville@etud.univ-jfc.fr>
2022-03-13 20:13:26 +0100

    Fix problems

--- src/main/java/ISIS/FIE4/MarieJeanEdouard/ISIS_Capitalist/WebService/WebserviceWorld.java
+++ src/main/java/ISIS/FIE4/MarieJeanEdouard/ISIS_Capitalist/WebService/WebserviceWorld.java
@@ -1,30 +1,41 @@
+
 package ISIS.FIE4.MarieJeanEdouard.ISIS_Capitalist.WebService;
+
 import ISIS.FIE4.MarieJeanEdouard.ISIS_Capitalist.tools.WorldServices;
 import generated.World;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import javax.xml.bind.JAXBException;
 
 @Path("generic")
 public class WebserviceWorld {
-    
+
     WorldServices services;
-    
-    public WebserviceWorld(){
-        services= new WorldServices();
+
+    public WebserviceWorld() {
+        services = new WorldServices();
     }
-    
+
     @GET
-    @Path("world") 
-    @Produces({"application/xml","application/json"})
-    public World getWorld(@Context HttpServletRequest request){
+    @Path("world")
+    @Produces({"application/xml", "application/json"})
+    public World getWorld(@Context HttpServletRequest request) throws JAXBException, FileNotFoundException{
         String username = request.getHeader("X-user");
         return services.getWorld(username);
     }
-    
+
+    @DELETE
+    @Path("world")
+    @Consumes({"application/xml", "application/json"})
+    public void deleteWorld(@Context HttpServletRequest request) {
+        String username = request.getHeader("X-user");
+        services.deleteworld(username);
+    }
 }
