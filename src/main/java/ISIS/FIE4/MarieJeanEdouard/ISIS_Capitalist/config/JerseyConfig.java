7d7ab8dc9a45283536cdf5710574776db040a79b
jemon <jean-edouard.monnerville@etud.univ-jfc.fr>
2022-03-13 20:12:47 +0100

    Fix problems

--- src/main/java/ISIS/FIE4/MarieJeanEdouard/ISIS_Capitalist/config/JerseyConfig.java
+++ src/main/java/ISIS/FIE4/MarieJeanEdouard/ISIS_Capitalist/config/JerseyConfig.java
@@ -5,7 +5,7 @@ import ISIS.FIE4.MarieJeanEdouard.ISIS_Capitalist.WebService.WebServiceAngelUpgr
 import ISIS.FIE4.MarieJeanEdouard.ISIS_Capitalist.WebService.WebServiceManager;
 import ISIS.FIE4.MarieJeanEdouard.ISIS_Capitalist.WebService.WebServiceProduct;
 import ISIS.FIE4.MarieJeanEdouard.ISIS_Capitalist.WebService.WebServiceUpgrade;
-import ISIS.FIE4.MarieJeanEdouard.ISIS_Capitalist.WebService.WebServiceWorld;
+import ISIS.FIE4.MarieJeanEdouard.ISIS_Capitalist.WebService.WebserviceWorld;
 import ISIS.FIE4.MarieJeanEdouard.ISIS_Capitalist.security.CORSResponseFilter;
 import javax.ws.rs.ApplicationPath;
 import org.glassfish.jersey.server.ResourceConfig;
@@ -16,7 +16,7 @@ import org.springframework.stereotype.Component;
 public class JerseyConfig extends ResourceConfig {
 
     public JerseyConfig() {
-        register(WebServiceWorld.class);
+        register(WebserviceWorld.class);
         register(WebServiceProduct.class);
         register(WebServiceManager.class);
         register(WebServiceUpgrade.class);
